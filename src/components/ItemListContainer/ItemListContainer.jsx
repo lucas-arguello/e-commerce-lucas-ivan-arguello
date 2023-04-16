c76f46a4c3bbb036ae77e71ab6a33039992000dc
Lucas Arguello <lucas5ivan@hotmail.com>
2023-04-16 13:32:03 -0300

    crear el stock de productos

--- src/components/ItemListContainer/ItemListContainer.jsx
+++ src/components/ItemListContainer/ItemListContainer.jsx
@@ -1,5 +1,22 @@
+import { useEffect } from "react"
+
+let miPromesa = new Promise ((res,rej)=>{
+
+  
+})
+
 
 export const ItemListContainer = ({greeting}) => {
+
+  useEffect(() => {
+
+      
+
+
+  })
+
+
+
   return (
     <div>
         <h2>{greeting}</h2>
